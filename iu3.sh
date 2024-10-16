8f9e6d82d8171dff8bff18f75f15dffc0099d006
t-site <sktruntime@hotmail.co.jp>
2024-10-17 01:55:47 +0900

    for ksh compatible

--- iu3.sh
+++ iu3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LINE=$(. rand.sh moso.txt 512 )
+LINE=$(. ./rand.sh moso.txt 512 )
 head -"${LINE}" moso.txt | tail -1
-LINE=$(. rand.sh em.txt 256 )
+LINE=$(. ./rand.sh em.txt 256 )
 head -"${LINE}" em.txt | tail -1
