79589bd784fdfda9bc09808f13e16fedf6278737
t-site <sktruntime@hotmail.co.jp>
2024-10-20 17:35:28 +0900

    patch unprovability

--- iu3.sh
+++ iu3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LINE=$(. ./rand.sh moso.txt 32768 )
+LINE=$(. ./rand.sh moso.txt )
 head -"${LINE}" moso.txt | tail -1
-LINE=$(. ./rand.sh em.txt 32768 )
+LINE=$(. ./rand.sh em.txt )
 head -"${LINE}" em.txt | tail -1
